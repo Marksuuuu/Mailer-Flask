3c17c06274e4964b67386917ab8b27e53af0c0b9
Marksuuuu <112992662+Marksuuuu@users.noreply.github.com>
2023-06-01 16:01:02 +0800

    Add Upload file function
    Add Upload file function

--- mailer-flask/main.py
+++ mailer-flask/main.py
@@ -1,11 +1,15 @@
 from flask import Flask, render_template, request, jsonify
 import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
+from email import encoders
 
 app = Flask(__name__)
 
 sender = 'yourSender@company.com'
 
-host = "192.168.1.1235" # <- HOST IP 
+host = "192.168.1.12345" # <- HOST IP 
 port = 25 # <- HOST PORT
 username = "yourUsername@company.com"
 password = "yourPassword"
@@ -15,25 +19,40 @@ def mail():
     fetch_getEmailContent = request.form['getEmailContent']
     fetch_getEmailReceiver = 'yourReceiver@company.com'  # DEFAULT RECEIVER CHANGE IT TO IF YOU WANT ----> IF YOU WANT TO SET AS DEFAULT OR ONLY ONE ##
     fetch_getEmailSender = request.form['getSenderEmail']
-    print(fetch_getEmailReceiver)
+    uploaded_file = request.files['file']  # Access the uploaded file
 
     if not fetch_getEmailContent or not fetch_getEmailSender:
         return jsonify(False)
     else:
-        sendingEmail(fetch_getEmailReceiver, fetch_getEmailSender, fetch_getEmailContent)
+        sendingEmail(fetch_getEmailReceiver, fetch_getEmailSender, fetch_getEmailContent, uploaded_file)
         return jsonify(True)
 
 
-def sendingEmail(fetch_getEmailReceiver, fetch_getEmailSender, fetch_getEmailContent):
+def sendingEmail(fetch_getEmailReceiver, fetch_getEmailSender, fetch_getEmailContent, uploaded_file):
     fromSender = f"<{fetch_getEmailSender}>"
-    message = f"""From: {fromSender}
-To: {fetch_getEmailReceiver}
-Subject: SMTP e-mail test
-{fetch_getEmailContent}"""
+
+    msg = MIMEMultipart()
+    msg['From'] = fromSender
+    msg['To'] = fetch_getEmailReceiver
+    msg['Subject'] = 'SMTP e-mail test'
+
+    body = fetch_getEmailContent
+    msg.attach(MIMEText(body, 'plain'))
+
+    # Attach the uploaded file
+    if uploaded_file:
+        filename = uploaded_file.filename
+        attachment = MIMEBase('application', 'octet-stream')
+        attachment.set_payload(uploaded_file.read())
+        encoders.encode_base64(attachment)
+        attachment.add_header('Content-Disposition', f'attachment; filename= {filename}')
+        msg.attach(attachment)
+
     try:
         smtpObj = smtplib.SMTP(host, port)
         smtpObj.login(username, password)  # Authenticate with the SMTP server
-        smtpObj.sendmail(sender, fetch_getEmailReceiver, message)
+        smtpObj.sendmail(sender, fetch_getEmailReceiver, msg.as_string())
+        
         msg = "Successfully sent email"
         print(msg)
         return jsonify(msg=msg)
--- mailer-flask/static/assets/js/customJs/customjs.js
+++ mailer-flask/static/assets/js/customJs/customjs.js
@@ -2,30 +2,38 @@ $(document).ready(function(){
 	$('#submit').click(function(){
 		getInputs()
 	});
+
 	function getInputs(){
-		var getEmailContent = $('#emailContent').val()
-		var getSenderEmail = $('#FromEmailAddress').val()
-		var pattern = /^\b[A-Z0-9._%-]+@[A-Z0-9.-]+\.[A-Z]{2,4}\b$/i
-		var mailContent = ''
-		if(!pattern.test(getSenderEmail))
-		{
-			$('#alert1-div').html('<div id="divAlert" class="alert alert-danger" role="alert"> Please Check your email.. Invalid Email Address </div>')
+		var getEmailContent = $('#emailContent').val();
+		var getSenderEmail = $('#FromEmailAddress').val();
+		var pattern = /^\b[A-Z0-9._%-]+@[A-Z0-9.-]+\.[A-Z]{2,4}\b$/i;
+		var mailContent = '';
+
+		if (!pattern.test(getSenderEmail)){
+			$('#alert1-div').html('<div id="divAlert" class="alert alert-danger" role="alert"> Please Check your email.. Invalid Email Address </div>');
 			return false;
-		}else if(getEmailContent == '' || getEmailContent == null){
-			$('#alert2-div').html('<div id="divAlert" class="alert alert-danger" role="alert"> Enter Some Text... </div>')
+		} else if (getEmailContent == '' || getEmailContent == null){
+			$('#alert2-div').html('<div id="divAlert" class="alert alert-danger" role="alert"> Enter Some Text... </div>');
 			return false;
-		}
-		else{
+		} else {
 			$('#alert1-div').empty();
 			$('#alert2-div').empty();
 		}
+
+		var formData = new FormData();
+		formData.append('getEmailContent', getEmailContent);
+		formData.append('getSenderEmail', getSenderEmail);
+		var fileInput = $('#emailAttachment')[0].files[0];
+		if (fileInput) {
+			formData.append('file', fileInput);
+		}
+
 		$.ajax({
 			url: '/sendEmail',
-			data: {
-				getEmailContent: getEmailContent,
-				getSenderEmail: getSenderEmail
-			},
+			data: formData,
 			method: 'POST',
+			contentType: false,
+			processData: false,
 			beforeSend: function () {
 				$('#waitme').waitMe({
 					effect: 'bounce',
@@ -46,22 +54,18 @@ $(document).ready(function(){
 						icon: 'error',
 						title: 'Error!',
 						showConfirmButton: true
-					})
-				}
-				else{
+					});
+				} else {
 					Swal.fire({
 						icon: 'success',
 						title: 'Sent!',
 						showConfirmButton: true
-					})
+					});
 				}
 			},
 
 		}).done(function(){
 			$('#waitme').waitMe('hide');
-
-		})
-
+		});
 	}
-
 });
